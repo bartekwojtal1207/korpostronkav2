20879528b1fdc5f00cb3728985212e2fcb749be0
Bartek <bartekwojtal1207@gmail.com>
2020-01-25 15:53:20 +0100

    zmiana jezyka

--- src/js/components/BestEmployers/BestEmoloyers.js
+++ src/js/components/BestEmployers/BestEmoloyers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SectionTitle from "../UI/Titles/SectionTitle";
+import img from './kincentric.jpg';
 
 const BestEmployers = () => {
 
@@ -7,14 +8,14 @@ const BestEmployers = () => {
         <section className={'best-employers'}>
             <div className={'container'}>
                 <div>
-                   <p>Porównywarka finansowa Totalmoney.pl została wyróżniona przez Kincentric tytułem:</p>
+                   <p className={'description'}>Porównywarka finansowa Totalmoney.pl została wyróżniona przez Kincentric tytułem:</p>
                     <SectionTitle>Najlepszy Pracodawca 2019 w Polsce!</SectionTitle>
                     <span>(Kincentric Best Employer)</span>
                     <p><strong>Kincentric Best Employers</strong> to program prowadzony i rozpoznawalny na świecie od 19 lat. Ma za zadanie wskazać firmy, które tworzą najbardziej angażujące miejsca pracy. Tytuł Najlepszego Pracodawcy jest przyznawany wyłącznie na podstawie wyników badań pracowników.</p>
                 </div>
                 <div>
                     <span className={'logo'}></span>
-
+                    <img src={img} alt="kincentric"/>
                 </div>
             </div>
         </section>
--- src/js/containers/homepage/Homepage.js
+++ src/js/containers/homepage/Homepage.js
@@ -4,12 +4,14 @@ import Numbers from "../../components/Numbers/Numbers";
 import Teams from "../../components/Teams/Teams";
 import JobsOffers from "../../components/JobsOffers/JobsOffers";
 import SendCv from "../../components/SendCv/SendCv";
+import BestEmployers from "../../components/BestEmployers/BestEmoloyers";
 
 class Homepage extends Component {
 
     render () {
         return (
             <div>
+                <BestEmployers/>
                 <WhatWeDo/>
                 <Numbers/>
                 <Teams/>
