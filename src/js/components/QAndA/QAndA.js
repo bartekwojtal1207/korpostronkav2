d868eef69a5d41101a02384c8aa556f2c3c271d7
Bartek <bartekwojtal1207@gmail.com>
2020-01-28 21:49:40 +0100

    career page mobile

--- src/js/components/QAndA/QAndA.js
+++ src/js/components/QAndA/QAndA.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SectionTitle from "../UI/Titles/SectionTitle";
 import data from './questions';
 
-const QAndA = () => {
-    const qAndA = data.qAndA;
-    const faqListItem = Object.keys(qAndA).map((value, index) => {
-            return (<li key={index}>
-                <span className={'icon'}>+</span>
-                <p className={'question'}>{qAndA[value].question}</p>
-                <p className={'answer'}>{qAndA[value].answer}</p>
-            </li>)
-    });
+class QAndA extends Component {
 
-    return (
-        <section className={'QAndA'}>
-            <header>
+    constructor(props) {
+        super(props)
+        this.state = {
+            qAnda: [...data.qAndA]
+        }
+
+    }
+
+     slideAnswer(index) {
+        console.log(index)
+    };
+
+    render() {
+        const qAndA = this.state.qAnda;
+        console.log(qAndA)
+
+        // const faqListItem = Object.keys(qAndA).map((value, index) => {
+        //     return (<li className={['questionItem', qAndA[value].show ? 'open' : null].join(' ')} onClick={() => this.slideAnswer(value)} key={value}>
+        //         <span className={'icon'}>+</span>
+        //         <p className={'question'}>{qAndA[value].question}</p>
+        //         <p className={'answer'}>{qAndA[value].answer}</p>
+        //     </li>)
+        // });
+
+        return (
+            <section className={'QAndA'}>
+                <header>
+                    <div className={'container'}>
+                        <SectionTitle>Pytania i odpowiedzi</SectionTitle>
+                    </div>
+                </header>
                 <div className={'container'}>
-                    <SectionTitle>Pytania i odpowiedzi</SectionTitle>
+                    <ul>
+                        {/*{faqListItem}*/}
+                    </ul>
                 </div>
-            </header>
-            <div className={'container'}>
-                <ul>
-                    {faqListItem}
-                </ul>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 };
 
 export default QAndA;
