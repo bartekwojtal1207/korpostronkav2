64412289924ff74e6ff1453a3a70bbab545d0ce7
Bartek <bartekwojtal1207@gmail.com>
2020-02-06 20:04:06 +0100

    dodanie bings maps

--- src/js/components/ContactMaps/ContactMaps.js
+++ src/js/components/ContactMaps/ContactMaps.js
@@ -6,7 +6,16 @@ const ContactMaps = () => {
     return (
         <ReactBingmaps
             bingmapKey = "AtgoirrF-zyM1W-6eej-oeFNA0aq6spIqKIJRQ3iNnH2XqSlQlrNyhWZ1lVSw_yM"
-            center = {[13.0827, 80.2707]}
+            center = {[51.1116416, 17.062197013531108]}
+            pushPins = {
+                [
+                    {
+                        "location":[51.1116416, 17.062197013531108], "option":{ color: 'black' }
+                    },
+                ]
+            }
+            mapTypeId = {"grayscale"}
+
         >
         </ReactBingmaps>
     )
